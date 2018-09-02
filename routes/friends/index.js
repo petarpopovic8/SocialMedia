53c0151d607177b6d6d9637730be6af52bd04727
Petar Popović <petar8p@gmail.com>
2018-09-02 12:30:57 +0200

    User adding functionality

--- routes/friends/index.js
+++ routes/friends/index.js
@@ -18,7 +18,26 @@ module.exports = {
 					return odgovor;
 				}
 			}
-		}])
+		},
+		{
+			method: "POST",
+			path: "/friend_request",
+			config: {
+				auth: "simple-cookie-strategy",
+				handler: async(request, h) => {
+					var odgovor = null;
+					await new Promise((resolve, reject) => User.find({"email": request.auth.credentials.user, function(err, sending_user){
+						User.find({"member_id" : request.payload.friend_member_id, function(err, added_user){
+							added_user[0].update({$push: {"friend_requests": {"member_id": sending_user[0].member_id, 
+							"friend_name": sending_user[0].name, "profile_pic": sending_user[0].user_profile[0].profile_pic}}})
+						}})
+						resolve();
+					}}));
+					return odgovor;
+				}
+			}
+		}
+		])
 	}
 }
 
